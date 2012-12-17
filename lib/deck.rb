bbcb85d29167d9624924b856c883928f8ac9cd3d
Stuart Fletcher <stuartf@subpop.com>
2012-12-17 12:59:12 -0800

    first commit

---
+++ lib/deck.rb
@@ -0,0 +1,12 @@
+class Deck
+  attr_reader :face_down, :discard  # piles
+
+  def initialize(num_decks = 2)
+  end
+
+  def shuffle
+  end
+
+  private
+
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,7 @@
+require 'rubygems'
+gem 'minitest'
+require 'minitest/unit'
+require 'minitest/autorun'
+
+# and then,
+# MiniTest::Unit::VERSION should be "4.3.3" (or more)
